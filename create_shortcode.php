5f166bbbbdf568db9466c7b63f95c680216fbde8
Mehmet Ali VATANLAR <33216260+vatanlar@users.noreply.github.com>
2022-05-09 17:19:31 +0300

    Create create_shortcode.php

---
+++ create_shortcode.php
@@ -0,0 +1,23 @@
+<?php 
+function wpb_list_child_pages() { 
+ 
+global $post; 
+ 
+if ( is_page() && $post->post_parent )
+ 
+    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
+else
+    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+ 
+if ( $childpages ) {
+ 
+    $string = '<ul class="wpb_page_list">' . $childpages . '</ul>';
+}
+ 
+return $string;
+ 
+}
+ 
+add_shortcode('wpb_childpages', 'wpb_list_child_pages');
+
+// [wpb_childpages]
